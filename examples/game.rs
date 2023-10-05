8a79f6af1ef8aa6cd5b884bf55da0657e8829ea6
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-10-05 09:53:17 +0530

    prelim webgl render limiters

--- examples/game.rs
+++ examples/game.rs
@@ -1,9 +1,37 @@
 fn main() {
-    greenify::set_event_handler(move |key| match key {
-        greenify::Key::Left => greenify::clear_screen_to_color(1.0, 0.0, 0.0, 1.0),
-        greenify::Key::Right => greenify::clear_screen_to_color(0.0, 1.0, 0.0, 1.0),
-        greenify::Key::Up => greenify::clear_screen_to_color(0.0, 0.0, 1.0, 1.0),
-        greenify::Key::Down => greenify::clear_screen_to_color(0.0, 1.0, 1.0, 1.0),
-        greenify::Key::Space => greenify::clear_screen_to_color(1.0, 1.0, 0.0, 1.0),
-    });
+    let mut x_position: f32 = 20.0;
+    let mut y_position: f32 = 30.0;
+
+    greenify::set_event_handler(move |key | {
+        let move_amount: f32 = 20.0;
+
+        match key {
+            greenify::Key::Left => {
+                if x_position - move_amount/2.0 >= 0.0 {
+                    x_position -= move_amount/2.0;
+                }
+            },
+            greenify::Key::Right => {
+                if !( x_position + move_amount/2.0 < 0.0) {
+                    x_position += move_amount/2.0;
+                }
+            },
+            greenify::Key::Up => {
+                if !(y_position + move_amount/2.0 <= 0.0){
+                    y_position += move_amount/2.0;
+                }
+            },
+            greenify::Key::Down => {
+                if y_position - move_amount/2.0 >= 0.0 {
+                    y_position -= move_amount/2.0;
+                }
+            },
+            greenify::Key::Space => {}
+        }
+
+        greenify::clear_screen_to_color(0.0, 0.0, 0.3, 1.0);
+        greenify::draw_rectangle(x_position, y_position, 10., 10.);
+    })  
+        
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,21 @@ pub enum Key {
     Space
 }
 
+pub enum Event {
+    KeyDown(Key),
+    Draw,
+}
+
 extern "C" {
     fn js_clear_screen_to_color(red: f32, green: f32, blue: f32, alpha: f32);
+    fn js_draw_rectangle(x: f32, y: f32, width: f32, height: f32);
 }
+
+#[no_mangle]
+pub extern "C" fn animate() {
+    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(Event::Draw))
+}
+
 #[no_mangle]
 pub extern "C" fn key_pressed(value: usize) {
     let key = match value {
@@ -20,8 +32,11 @@ pub extern "C" fn key_pressed(value: usize) {
         _ => return,
     };
 
-    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(key))
+    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(Event::KeyDown(key)))
 }
+
+
+
 
 pub fn clear_screen_to_color(red: f32, green: f32, blue: f32, alpha: f32) {
     unsafe {
@@ -29,11 +44,17 @@ pub fn clear_screen_to_color(red: f32, green: f32, blue: f32, alpha: f32) {
     }
 }
 
-thread_local! {
-    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut(Key)>> = std::cell::RefCell::new(Box::new(|_|{}));
+pub fn draw_rectangle(x: f32, y: f32, width: f32, height: f32) {
+    unsafe {
+        js_draw_rectangle(x, y, width, height);
+    }
 }
 
-pub fn set_event_handler(function: impl FnMut(Key) + 'static) {
+thread_local! {
+    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut(Event)>> = std::cell::RefCell::new(Box::new(|_|{}));
+}
+
+pub fn set_event_handler(function: impl FnMut(Event) + 'static) {
 /*...*/
     EVENT_HANDLER.with(|event_handler| {
         *event_handler.borrow_mut() = Box::new(function);
